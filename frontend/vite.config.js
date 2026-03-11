6c9c29d8c9f99af5d079409b6a173cb2287a7c7f
vrkljam <vrkljam@earthlink.net>
2026-03-10 20:37:39 -0600

    changes to vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/static/", // <-- ensures all assets are served under Django's STATIC_URL
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // build folder
+    assetsDir: "assets", // your current folder structure matches this
+  },
+});
