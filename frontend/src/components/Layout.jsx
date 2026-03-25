f2027113288d7d30329b3052687b51993d116c66
vrkljam <vrkljam@earthlink.net>
2026-03-25 09:57:42 -0600

    navbar updates

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -8,6 +8,7 @@ const Layout = ({ theme, setTheme }) => {
   return (
     <div className="d-flex flex-column vh-100">
       <Navbar theme={theme} setTheme={setTheme} />
+
       <div className="d-flex flex-grow-1">
         <Sidebar />
         <main className="flex-grow-1">
